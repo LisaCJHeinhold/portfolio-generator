83b6e67e4a71a2d1fd1c8c702426a53bb825bba1
Lisa <lisacjheinhold@gmail.com>
2022-07-13 22:18:54 -0600

    first question

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+const inquirer = require('inquirer');
+// import inquirer from 'inquirer'
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
